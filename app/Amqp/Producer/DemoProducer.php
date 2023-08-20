6a516d7dba6ae52466d2a43b4304569e138fa2a0
baoyuan <yangbaoyuan0304@163.com>
2023-08-20 13:53:50 +0800

    git

--- app/Amqp/Producer/DemoProducer.php
+++ app/Amqp/Producer/DemoProducer.php
@@ -7,7 +7,7 @@ namespace App\Amqp\Producer;
 use Hyperf\Amqp\Annotation\Producer;
 use Hyperf\Amqp\Message\ProducerMessage;
 
-#[Producer(exchange: 'hyperf', routingKey: 'hyperf')]
+#[Producer(exchange: 'hyperf', routingKey: 'test')]
 class DemoProducer extends ProducerMessage
 {
     public function __construct($data)
